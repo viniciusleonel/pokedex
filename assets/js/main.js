5782ad2d2e6e958844f9d7332bb0ace536b3701e
Vinicius Leonel <viniciuslps.cms@gmail.com>
2023-08-21 16:37:29 -0300

    criando uma nova lista html com o map

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,27 @@
 
-console.log("funcionou");
+function convertPokemonToList(pokemon) {
+    return `
+        <li class="pokemon pokebola-img">
+                    
+            <span class="pokemon-number">#001</span>
+            <span class="pokemon-name">${pokemon.name}</span>
 
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+            <div class="pokemon-details">
+                <ol class="pokemon-types">
+                    <li class="pokemon-type">Grass</li>
+                    <li class="pokemon-type">Poison</li>
+                </ol>
+                
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                
+            </div>
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.log(error))
+        </li>       
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons) => { 
+    pokemonList.innerHTML += pokemons.map(convertPokemonToList).join(" ")
+})    
