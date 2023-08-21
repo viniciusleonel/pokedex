d3e47f14babe4a7272bc4d4d0526b25f8ed14846
Vinicius Leonel <viniciuslps.cms@gmail.com>
2023-08-20 21:11:40 -0300

    pegando dados da api com fetch, .then

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,11 @@
 
 console.log("funcionou");
+
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
